754f2f572128def0fa9e883089469329c86c400a
Barbara <40583024+barbaraguarino@users.noreply.github.com>
2026-06-14 23:03:58 -0300

    ...

---
+++ src/config.py
@@ -0,0 +1,15 @@
+HORARIOS_DISPONIVEIS = [
+    "Segunda_07h", "Segunda_09h", "Segunda_11h", "Segunda_14h", "Segunda_16h", "Segunda_18h", "Segunda_20h",
+    "Terca_07h", "Terca_09h", "Terca_11h", "Terca_14h", "Terca_16h", "Terca_18h", "Terca_20h",
+    "Quarta_07h", "Quarta_09h", "Quarta_11h", "Quarta_14h", "Quarta_16h", "Quarta_18h", "Quarta_20h",
+    "Quinta_07h", "Quinta_09h", "Quinta_11h", "Quinta_14h", "Quinta_16h", "Quinta_18h", "Quinta_20h",
+    "Sexta_07h", "Sexta_09h", "Sexta_11h", "Sexta_14h", "Sexta_16h", "Sexta_18h", "Sexta_20h",
+]
+
+TURNOS_MANHA = [h for h in HORARIOS_DISPONIVEIS if "07h" in h or "09h" in h or "11h" in h]
+
+HORARIOS_PROIBIDOS = ["Quarta_16h"]
+
+HORARIOS_RUINS = ["Segunda_07h", "Sexta_18h", "Sexta_20h"]
+
+PESO_HARD = 1000
---
+++ src/fitness_evaluator.py
@@ -0,0 +1,122 @@
+from collections import defaultdict
+from typing import Dict, List
+from domain.models import Cromossomo, Professor
+import config
+
+
+class FitnessEvaluator:
+
+    def __init__(self, professores: Dict[str, Professor]):
+        self.professores = professores
+
+    def calcular_fitness(self, cromossomo: Cromossomo) -> int:
+        penalidade_total = 0
+
+        alocacoes_por_horario = defaultdict(list)
+        alocacoes_por_docente_dia = defaultdict(list)
+        alocacoes_por_periodo_dia = defaultdict(list)
+
+        for gene in cromossomo.genes:
+            alocacoes_por_horario[gene.horario].append(gene)
+
+            dia = gene.horario.split("_")[0]
+            alocacoes_por_docente_dia[(gene.disciplina.id_professor, dia)].append(gene)
+            alocacoes_por_periodo_dia[(gene.disciplina.periodo, dia)].append(gene)
+
+        penalidade_total += self._avaliar_hc_por_horario(alocacoes_por_horario)
+        penalidade_total += self._avaliar_hc_disponibilidade(cromossomo.genes)
+
+        penalidade_total += self._avaliar_sc_infraestrutura(cromossomo.genes)
+        penalidade_total += self._avaliar_sc_pedagogia(cromossomo.genes)
+        penalidade_total += self._avaliar_sc_jornada_docente(alocacoes_por_docente_dia)
+        penalidade_total += self._avaliar_sc_jornada_aluno(alocacoes_por_periodo_dia)
+        penalidade_total += self._avaliar_sc_equidade(cromossomo.genes)
+
+        cromossomo.fitness = penalidade_total
+        return penalidade_total
+
+
+    def _avaliar_hc_por_horario(self, alocacoes_por_horario: dict) -> int:
+        penalidade = 0
+        for horario, genes in alocacoes_por_horario.items():
+            professores_vistos = set()
+            salas_vistas = set()
+            periodos_vistos = set()
+
+            if horario in config.HORARIOS_PROIBIDOS:
+                penalidade += config.PESO_HARD * len(genes)
+
+            for gene in genes:
+                if gene.disciplina.id_professor in professores_vistos:
+                    penalidade += config.PESO_HARD
+                professores_vistos.add(gene.disciplina.id_professor)
+
+                if gene.sala.id in salas_vistas:
+                    penalidade += config.PESO_HARD
+                salas_vistas.add(gene.sala.id)
+
+                if gene.disciplina.periodo in periodos_vistos:
+                    penalidade += config.PESO_HARD
+                periodos_vistos.add(gene.disciplina.periodo)
+
+        return penalidade
+
+    def _avaliar_hc_disponibilidade(self, genes: List) -> int:
+        penalidade = 0
+        for gene in genes:
+            prof = self.professores.get(gene.disciplina.id_professor)
+            if prof and not prof.is_disponivel(gene.horario):
+                penalidade += config.PESO_HARD
+        return penalidade
+
+    def _avaliar_sc_infraestrutura(self, genes: List) -> int:
+        penalidade = 0
+        for gene in genes:
+            excedente = gene.disciplina.numero_alunos - gene.sala.capacidade_maxima
+            if excedente > 0:
+                penalidade += excedente * 10
+
+            vazios = gene.sala.capacidade_maxima - gene.disciplina.numero_alunos
+            if vazios > 20:
+                penalidade += vazios * 5
+
+            if gene.disciplina.is_alta_demanda and gene.sala.capacidade_maxima < 40:
+                penalidade += 50
+        return penalidade
+
+    def _avaliar_sc_pedagogia(self, genes: List) -> int:
+        penalidade = 0
+        for gene in genes:
+            if gene.disciplina.is_preferencia and gene.horario not in config.TURNOS_MANHA:
+                penalidade += 40
+        return penalidade
+
+    def _avaliar_sc_jornada_docente(self, alocacoes_por_docente_dia: dict) -> int:
+        penalidade = 0
+        for (id_prof, dia), genes_no_dia in alocacoes_por_docente_dia.items():
+            if len(genes_no_dia) > 2:
+                penalidade += 600
+        return penalidade
+
+    def _avaliar_sc_jornada_aluno(self, alocacoes_por_periodo_dia: dict) -> int:
+        penalidade = 0
+        for (periodo, dia), genes_no_dia in alocacoes_por_periodo_dia.items():
+            if len(genes_no_dia) > 3:
+                penalidade += 30
+
+            if len(genes_no_dia) >= 2:
+                horarios_ordenados = sorted([g.horario for g in genes_no_dia])
+                penalidade += 20
+        return penalidade
+
+    def _avaliar_sc_equidade(self, genes: List) -> int:
+        penalidade = 0
+        horarios_ruins_por_prof = defaultdict(int)
+        for gene in genes:
+            if gene.horario in config.HORARIOS_RUINS:
+                horarios_ruins_por_prof[gene.disciplina.id_professor] += 1
+
+        for id_prof, contagem in horarios_ruins_por_prof.items():
+            if contagem > 1:
+                penalidade += 500
+        return penalidade
